9b35390f91de3fa12fdc9462801b129f2bb18078
寒幽月 <141236517+hanunq@users.noreply.github.com>
2023-08-04 13:53:46 +0800

    Create hanunq.js

---
+++ public/hanunq.js
@@ -0,0 +1,9 @@
+new Valine({
+    el: '#comments',
+    appId: 'Your Valine appid',
+    appKey: 'Your Valine appkey',
+    placeholder: '请填写评论...',
+    notify: false,
+    verify: false,
+    avatar: ''
+  });
